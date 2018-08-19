1a3bfac4e7ced64404af62c32c2160edfd6ed9d0
Mucyo Miller <mucyomiller@gmail.com>
2018-08-19 03:25:27 +0200

    adds unaryInterceptor & streamInterceptor & auth func  to intercept and validate jwt token

--- main.go
+++ main.go
@@ -1,13 +1,87 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	pb "github.com/mucyomiller/hahiye/hahiye"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
 	fmt.Println("gprc service...")
 	_ = &pb.Place{}
 
+}
+
+// authUnaryIntercept intercepts incoming requests to validate
+// jwt token from metadata header "authorization"
+func authUnaryIntercept(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (resp interface{}, err error) {
+	if err := auth(ctx); err != nil {
+		return nil, err
+	}
+	log.Println("authorization OK")
+	return handler(ctx, req)
+}
+
+// streamAuthIntercept intercepts to validate authorization
+func streamAuthIntercept(
+	server interface{},
+	stream grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler,
+) error {
+	if err := auth(stream.Context()); err != nil {
+		return err
+	}
+	log.Println("authorization OK")
+	return handler(server, stream)
+}
+
+func auth(ctx context.Context) error {
+	meta, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Errorf(
+			codes.InvalidArgument,
+			"missing context",
+		)
+	}
+
+	authString, ok := meta["authorization"]
+	if !ok {
+		return status.Errorf(
+			codes.Unauthenticated,
+			"missing authorization",
+		)
+	}
+	// validate token algo
+	log.Println("found jwt token")
+	jwtToken, err := jwt.Parse(
+		authString[0],
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("bad signing method")
+			}
+			// additional validation goes here.
+			return []byte("s3cr3t"), nil
+		},
+	)
+
+	if jwtToken.Valid {
+		return nil
+	}
+	if err != nil {
+		return status.Error(codes.Internal, err.Error())
+	}
+	return status.Error(codes.Internal, "bad token")
 }
